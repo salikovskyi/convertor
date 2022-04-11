3d0921aa6e8c6e49633f8b40a8f7c8a30c421b5f
salikovskyi <osalikovsiy@gmail.com>
2022-04-11 22:50:15 +0300

    done

---
+++ src/components/Currency/Currency.js
@@ -0,0 +1,21 @@
+import css from './Currency.module.css'
+import PropTypes from 'prop-types'
+
+export default function CurrencyInp(props) {
+    return (
+        <div className={css.group}> 
+      <input type="text" value={props.amount} onChange={ev => props.onAmountChange(ev.target.value)} />
+      <select value={props.currency} onChange={ev => props.onCurrencyChange(ev.target.value)}>
+        {props.currencies.map((currency => (
+          <option value={currency}>{currency}</option>
+        )))}
+      </select>
+    </div>
+    )
+}
+
+CurrencyInp.propTypes = {
+    amount: PropTypes.number.isRequired,
+    currency: PropTypes.string.isRequired,
+    currencies: PropTypes.array,
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,37 @@
+import css from "./Header.module.css";
+
+export default function Header({ rates }) {
+  const currentDate = new Date().toLocaleString("ua", {
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+  });
+  return (
+    <div className={css.header}>
+             <h1 className={css.title}>Currency Converter</h1>
+      <h1 className={css.subtitle}>Exchange rate for <span className={css.subtitle_data}>{currentDate}</span></h1>
+      <ul className={css.currency_list}>
+        <li className={css.currency}>
+          <h2>USD/EUR</h2>
+          <p>{rates.USD}</p>
+        </li>
+        <li className={css.currency}>
+          <h2>UAH/EUR</h2>
+          <p>{rates.UAH}</p>
+        </li>
+        <li className={css.currency}>
+          <h2>CHF/EUR</h2>
+          <p>{rates.CHF}</p>
+        </li>
+        <li className={css.currency}>
+          <h2>JPY/EUR</h2>
+          <p>{rates.JPY}</p>
+        </li>
+        <li className={css.currency}>
+          <h2>GBP/EUR</h2>
+          <p>{rates.GBP}</p>
+        </li>
+      </ul>
+    </div>
+  );
+}
